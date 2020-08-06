d421254b81febf4b85d2b8779c343e5a1cac9f7a
RajmiNakarmi <68827515+RajmiNakarmi@users.noreply.github.com>
2020-08-05 20:37:48 -0400

    Update hello_Rajmi.java

--- src/hello_Rajmi.java
+++ src/hello_Rajmi.java
@@ -3,5 +3,7 @@ import java.sql.SQLOutput;
 public class hello_Rajmi {
     public static void main(String[] args) {
         System.out.println(" K cha , Khana khaio ?");
+        
+        // dheari qyestion cha..
     }
 }
